1f57937f23f8d45f901dce030d3588930c37aef4
sane <saneschio@gmail.com>
2016-07-25 17:05:53 -0400

    cadastrarusuario ui

--- src/com/br/ufms/schirrel/panels/CadastrarUsuario.java
+++ src/com/br/ufms/schirrel/panels/CadastrarUsuario.java
@@ -9,13 +9,15 @@ import java.sql.SQLException;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
 import javax.swing.text.AbstractDocument;
 import javax.swing.text.DocumentFilter;
 
+import com.br.ufms.schirrel.UI.EUButton;
+import com.br.ufms.schirrel.UI.EUTextField;
 import com.br.ufms.schirrel.banco.DAO;
 
 public class CadastrarUsuario extends JPanel implements ActionListener {
@@ -31,46 +33,53 @@ public class CadastrarUsuario extends JPanel implements ActionListener {
 
 	public CadastrarUsuario(DAO D) {
 		dao = D;
-		setBounds(0, 60, 800, 300);
+		setBounds(0, 60, 795, 300);
 		setLayout(null);
-		setBorder(new TitledBorder(null, "Cadastrar Item", TitledBorder.LEADING, TitledBorder.CENTER, null, null));
+		setBorder(new TitledBorder(null, "Cadastrar Item", TitledBorder.LEADING, TitledBorder.CENTER, null, new Color(24, 135, 180)));
 	
 
 		setBackground(Color.WHITE);
 
-		JLabel lblUsuario = new JLabel("Usuario: ");
-		lblUsuario.setBounds(10, 30, 140, 20);
-		lblUsuario.setFont(new Font("Arial", Font.BOLD, 18));
-		add(lblUsuario);
+		JPanel panelu = new JPanel();
+		panelu.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Usuario",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(24, 135, 180)));
+		panelu.setBounds(10, 17, 780, 60);
+		panelu.setLayout(null);
+		panelu.setBackground(Color.WHITE);
+		add(panelu);
 
-		tfUsuario = new JTextField();
-		tfUsuario.setBounds(160, 30, 140, 26);
+		tfUsuario = new EUTextField();
+		tfUsuario.setBounds(15, 20, 750, 25);
 		tfUsuario.setFont(new Font("Arial", Font.BOLD, 18));
-		add(tfUsuario);
+		panelu.add(tfUsuario);
 		tfUsuario.setColumns(10);
 
-		JLabel lblRegistro = new JLabel("Registro: ");
-		lblRegistro.setBounds(10, 70, 140, 20);
-		lblRegistro.setFont(new Font("Arial", Font.BOLD, 18));
-		add(lblRegistro);
+		JPanel panelr = new JPanel();
+		panelr.setBorder(new TitledBorder(new LineBorder(new Color(184, 207, 229)), "Registro",
+				TitledBorder.LEADING, TitledBorder.TOP, null, new Color(24, 135, 180)));
+		panelr.setBounds(10, 90, 780, 60);
+		panelr.setLayout(null);
+		panelr.setBackground(Color.WHITE);
+		add(panelr);
 
-		tfRegistro = new JTextField();
-		tfRegistro.setBounds(160, 70, 140, 26);
+		tfRegistro = new EUTextField();
+		tfRegistro.setBounds(15, 20, 750, 25);
 		tfRegistro.setFont(new Font("Arial", Font.BOLD, 18));
 		AbstractDocument document = (AbstractDocument) tfRegistro.getDocument();
 		document.setDocumentFilter(filter);
 		tfRegistro.setDocument(document);
-		add(tfRegistro);
+		panelr.add(tfRegistro);
 		tfRegistro.setColumns(10);
 
 		lblStatus = new JLabel("");
 		lblStatus.setBounds(100, 230, 600, 20);
+		lblStatus.setForeground(new Color(24, 135, 180));
 		lblStatus.setFont(new Font("Arial", Font.BOLD, 16));
 		lblStatus.setHorizontalAlignment(SwingConstants.CENTER);
 		add(lblStatus);
 
-		btCadastrar = new JButton("Cadastrar");
-		btCadastrar.setBounds(160, 190, 130, 30);
+		btCadastrar = new EUButton("Cadastrar");
+		btCadastrar.setBounds(635, 250, 150, 40);
 		btCadastrar.addActionListener(this);
 		add(btCadastrar);
 
